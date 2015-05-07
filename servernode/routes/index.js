5eea62fed220d55dd055e3332d986d2436801275
Erik Weilow <nissehutt@gmail.com>
2015-05-07 19:33:49 +0200

    Template data added

--- servernode/routes/index.js
+++ servernode/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var templatedata = require("../templatedata.js");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Our Little Project' });
+	res.render('index', { title: 'Our Little Project', data: templatedata.mockup });
 });
 
 module.exports = router;
---
+++ servernode/templatedata.js
@@ -0,0 +1,30 @@
+module.exports.mockup = [
+  {
+    ID: 5,
+    NiceName: "Coke-Rainbow",
+    Readings: [ 22, 22.1, 22.15, 22.1, 22 ],
+    LastReading: 3.1,
+    Interval: 2.0
+  },
+  {
+    ID: 7,
+    NiceName: "Sprint-Wall",
+    Readings: [ 23, 22.5, 22, 22.1, 27 ],
+    LastReading: 17.3,
+    Interval: 2.0
+  },
+  {
+    ID: 3,
+    NiceName: "Foam-Man",
+    Readings: [ 22.0, 22.0, 22.01, 26.4, 75.1 ],
+    LastReading: 1.1,
+    Interval: 2.0
+  },
+  {
+    ID: 7,
+    NiceName: "Får-Napoleon",
+    Readings: [ 22, 22.5, 22, 22.1, 22.05 ],
+    LastReading: 125.3,
+    Interval: 1.0
+  }
+];
