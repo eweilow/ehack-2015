45e1e59fa0ef2dae0755c25cc85576893396469b
Kristoffer Gunnarsson <kristoffergunnarsson47@gmail.com>
2015-05-07 20:38:01 +0200

    Fixed node ID stuff

--- servernode/routes/index.js
+++ servernode/routes/index.js
@@ -8,4 +8,13 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Our Little Project', data: templatedata.mockup });
 });
 
+router.get("/node/:id", function(req, res){
+	var id = req.params.id;
+	
+	var node = templatedata[id];
+	if(!node) return res.status(404);
+	
+	res.render("node", {node: node});
+});
+
 module.exports = router;
