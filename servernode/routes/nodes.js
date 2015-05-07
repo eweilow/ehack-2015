2a9b9d73a7c9652980d1a6b14929433a32d1bf07
Tobba <tobias.haegermarck@gmail.com>
2015-05-08 00:12:17 +0200

    Node view now gets real data

--- servernode/routes/nodes.js
+++ servernode/routes/nodes.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var templatedata = require('../templatedata');
 
-router.get("/:id", function(req, res){
-	var id = req.params.id;
+router.get('/:node_id', function(req, res, next) {
+	var node_id = req.params.id;
 	
-	var node = templatedata.mockup[id];
-	if(!node) { res.status(404); return res.end(); }
-	
-	res.render("nodes", {node: node});
+	var connection = req.app.get('connection');
+	connection.query("SELECT datapoint.* FROM temperaturereading datapoint INNER JOIN (SELECT nodeid, id, MAX(unixmilliseconds) time FROM temperaturereading WHERE id = ? GROUP BY nodeid) maxdata WHERE datapoint.nodeid = maxdata.nodeid AND datapoint.id = maxdata.id", [req.params.node_id], function(err, data) {
+		if (!data.length) {
+			res.status(500);
+			res.end();
+			return;
+		}
+		var sensors = {}
+		data.forEach(function(sensor_data) {
+			sensors[sensor_data.nodeid] = { value: sensor_data.reading };
+		});
+		res.render("nodes", { sensors: sensors });
+	});
 });
 
 module.exports = router;
