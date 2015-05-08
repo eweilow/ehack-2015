b2ed6cf8a79e3cedfa2f4da02107c593adfb435f
Tobba <tobias.haegermarck@gmail.com>
2015-05-08 11:42:16 +0200

    Fix broken panicked code

--- servernode/routes/node.js
+++ servernode/routes/node.js
@@ -6,12 +6,12 @@ var async = require('async');
 router.get('/:node_id', function(req, res, next) {
 	var node_id = req.params.node_id;
 	
-  console.log(nodes.list);
-  var node = null;  
-  for (var key in nodes.list)
-  {
-    if (nodes.list[key].id == node_id) node = nodes.list[key];
-  }
+	console.log(nodes.list);
+	var node = null;  
+	for (var key in nodes.list)
+	{
+		if (nodes.list[key].id == node_id) node = nodes.list[key];
+	}
 	
 	var connection = req.app.get('connection');
 	var sensor_queries = {};
@@ -39,24 +39,29 @@ router.get('/:node_id', function(req, res, next) {
 router.post("/:node_id/", function(req, res) {
 	var node_id = req.params.node_id;
 	
-	var node = nodes.list[0];
+	console.log(nodes.list);
+	var node = null;  
+	for (var key in nodes.list)
+	{
+		if (nodes.list[key].id == node_id) node = nodes.list[key];
+	}
 	
 	var connection = req.app.get('connection');
 	var sensor_queries = {};
 	for (var key in node.sensors) {
 		var sensor = node.sensors[key];
-		var sensor_id = parseInt(key);
-		sensor_queries[sensor_id] = function(sensor, sensor_id) {
+		
+		sensor_queries[key] = function(sensor, key) {
 			return function(callback) {
-				nodes.read_sensor(sensor_id, sensor.type, function(data) {
+				nodes.read_sensor(node_id, sensor.type, function(data) {
 					if (data) {
-						callback(null, { type: sensor.type,  value: data });
+						callback(null, { type: sensor.type, value: data });
 					} else {
 						callback(null, { type: 0,  value: "" });
 					}
 				});
 			};
-		}(sensor, sensor_id);
+		}(sensor, key);
 	}
 	
 	async.parallel(sensor_queries, function(err, sensors) {
