6c0a71860daabce516ea2fb53972a6e6c61cefe0
obskyr <powpowd@gmail.com>
2015-05-08 07:28:53 +0200

    Better error handling, config updating.
    Hot-swapping! Whoaaa!

--- sensornode/tcpcommands.py
+++ sensornode/tcpcommands.py
@@ -1,34 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+import struct
 import SocketServer
+import time
+from collections import Mapping
+from threading import Thread
 
 HOST = "0.0.0.0"
-PORT = 5002
+PORT = 5048
 
 commandHandlers = {}
 
-def pingHandler(request):
+def pingHandler(request, node):
     request.sendall(chr(0))
 
 commandHandlers[0] = pingHandler
 
-class CommandHandler(SocketServer.BaseRequestHandler):
-    def handle(self):
-        self.data = self.request.recv(1024)
+def configHandler(request, node):
+    dataLength = struct.unpack('<I', request.recv(4))[0]
+    configJson = request.recv(dataLength)
+    config = json.loads(configJson)
 
-        print "Received data:"
-        print self.data.__repr__()
+    updateRecursiveDictionary(node.config, config)
 
-        commandHandlers[ord(self.data)](self.request)
+commandHandlers[1] = configHandler
 
-if __name__ == '__main__':
-    import sys
+def updateRecursiveDictionary(d1, d2):
+    for key, value in d2.iteritems():
+        if isinstance(value, Mapping):
+            subDict = updateRecursiveDictionary(d1.get(key, {}), value)
+            d1[key] = subDict
+        else:
+            d1[key] = value
+    return d1
 
-    try:
-        port = int(sys.argv[1])
-    except (IndexError, ValueError):
-        port = PORT
+class Commander(object):
+    def __init__(self, node, host=HOST, port=PORT):
+        self.node = node
+        self.server = SocketServer.TCPServer((host, port), self.handler())
 
-    server = SocketServer.TCPServer((HOST, port), CommandHandler)
-    server.serve_forever()
+    commandHandlers = commandHandlers
+    exitSignalReceived = False
+
+    def handler(self):
+        commander = self
+        class CommandHandler(SocketServer.BaseRequestHandler):
+            def handle(self):
+                data = self.request.recv(1)
+                print "Received command {}. Executing.".format(str(ord(data)))
+
+                commander.node.
+
+                commandHandlers[ord(data)](self.request, commander.node)
+        return CommandHandler
+
+    def run(self):
+        Thread(target=self.server.serve_forever).start()
+
+    def close(self):
+        self.server.shutdown()
